b730192912746ec4c3709fc7ff8180524eb12420
Avinash R <nashpapa@gmail.com>
2016-08-23 19:36:50 +0530

    add button to copy the token

---
+++ app/src/main/java/com/sample/samplemessaging/FirebaseModel.java
@@ -0,0 +1,35 @@
+package com.sample.samplemessaging;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class FirebaseModel {
+
+    private final ClipboardManager clipboard;
+
+    private final Context context;
+
+    FirebaseModel(Context context) {
+        this.context = context;
+        clipboard = (ClipboardManager) this.context.getSystemService(Context.CLIPBOARD_SERVICE);
+    }
+
+    public void openConsoleWithTrackingId() {
+        String token = FirebaseInstanceId.getInstance().getToken();
+        ClipData clip = ClipData.newPlainText("Firebase Token", token);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(context, context.getString(R.string.main_screen_token_copied), LENGTH_SHORT)
+                .show();
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("https://console.firebase.google.com/"));
+        context.startActivity(intent);
+    }
+}
--- app/src/main/java/com/sample/samplemessaging/MainActivity.java
+++ app/src/main/java/com/sample/samplemessaging/MainActivity.java
@@ -1,14 +1,18 @@
 package com.sample.samplemessaging;
 
+import com.sample.samplemessaging.databinding.ActivityMainBinding;
+
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main);
+        binding.setFirebaseModel(new FirebaseModel(this));
     }
 }
